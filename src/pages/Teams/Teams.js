9b1a7134e00116969ed102e25cfc90a8b98a669d
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-07-08 14:01:55 +0000

    Shifted team display o tTaam component

--- src/pages/Teams/Teams.js
+++ src/pages/Teams/Teams.js
@@ -1,33 +1,17 @@
 import React from "react";
-import axios from "axios";
 import Team from "./components/Team";
 
 export default class Teams extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Team />
-                {
-                    this.props.teams.map(t => {
-                        return (
-                            <React.Fragment>
-                                <h3>{t.team_name}</h3>
-                                <ul>
-                                    {t.team_composition.map(m => {
-                                        return (
-                                            <li>
-                                                <img src={require(`../../images/characters/icons/${this.props.allCharacters.filter(c => c._id === m.character.$oid)[0].value}_icon.webp`)} alt="" style={{backgroundColor: "gold"}}/>
-                                                <h5>
-                                                    {this.props.allCharacters.filter(c => c._id === m.character.$oid)[0].display}
-                                                </h5>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </React.Fragment>
-                        )
-                    })
-                }
+                <Team
+                    teams={this.props.teams}
+                    allCharacters={this.props.allCharacters}
+                    allWeapons={this.props.allWeapons}
+                    allArtifcats={this.props.allArtifcats}
+                    allBosses={this.props.allBosses}
+                />
             </React.Fragment>
         )
     }
