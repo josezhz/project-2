a84fc4a4d018167901894a02a403e51cbbec13fc
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-07-08 07:09:39 +0000

    Added pictures of artifacts, bosses, characters, elements, rarity, and weapon types

--- src/pages/Teams/Teams.js
+++ src/pages/Teams/Teams.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default class Teams extends React.Component {
     state = {
-        teams: []
+        teams: [],
     };
 
     allCharacters = [];
@@ -12,7 +12,7 @@ export default class Teams extends React.Component {
     allBosses = [];
 
     async componentDidMount() {
-        const BASE_URI = "https://3000-josezhz-project2api-rly1361e77s.ws-us52.gitpod.io/";
+        const BASE_URI = "https://giteams.herokuapp.com/";
 
         let reqCharacters = axios.get(BASE_URI + "characters");
         let reqWeapons = axios.get(BASE_URI + "weapons");
@@ -25,7 +25,7 @@ export default class Teams extends React.Component {
         this.allCharacters = resCharacters.data;
         this.allWeapons = resWeapons.data;
         this.allArtifacts = resArtifacts.data;
-        this.allBosses = resBosses.data;
+        this.allBosses = resBosses.data.filter(b => b.type === "weekly");
 
         let resTeams = await axios.get(BASE_URI + "teams");
         this.setState({
@@ -37,8 +37,51 @@ export default class Teams extends React.Component {
         return (
             <React.Fragment>
                 {
+                    this.state.teams.map(t => {
+                        return (<div>{t.team_name}</div>)
+                    })
+                }
+                <hr />
+                {
                     this.allCharacters.map(c => {
-                        return (<div>{c.display}</div>)
+                        return (
+                            <React.Fragment>
+                                <img src={require(`../../images/characters/icons/${c.value}_icon.webp`)} alt="" style={{backgroundColor: "gold"}}/>
+                                <div>{c.display}</div>
+                            </React.Fragment>
+                        )
+                    })
+                }
+                <hr />
+                {
+                    this.allArtifacts.map(a => {
+                        return (
+                            <React.Fragment>
+                                <img src={require(`../../images/artifacts/${a.value}.webp`)} alt="" style={{backgroundColor: "purple"}}/>
+                                <div>{a.display}</div>
+                            </React.Fragment>
+                        )
+                    })
+                }
+                <hr />
+                {
+                    this.allBosses.map(b => {
+                        return (
+                            <React.Fragment>
+                                <img src={require(`../../images/bosses/${b.value}.webp`)} alt="" style={{backgroundColor: "gold"}}/>
+                                <div>{b.display}</div>
+                            </React.Fragment>
+                        )
+                    })
+                }
+                <hr />
+                {
+                    this.allWeapons.map(w => {
+                        return (
+                            <React.Fragment>
+                                <div>{w.value}</div>
+                            </React.Fragment>
+                        )
                     })
                 }
             </React.Fragment>
