3e09a8035685a68d411e33f0777c6df05e7dfa53
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-07-10 14:51:34 +0000

    Added responsive filter section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min"
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 import Teams from "./pages/Teams/Teams";
 import AddTeam from "./pages/AddTeam/AddTeam";
@@ -10,7 +10,8 @@ class App extends React.Component {
   state = {
     teams: [],
     active: "explore",
-    navbarCollapsed: true
+    navbarHidden: true,
+    filterHidden: true
   }
 
   allCharacters = [];
@@ -59,24 +60,24 @@ class App extends React.Component {
               <button className="navbar-brand border-0 bg-none ms-md-3" onClick={() => { this.setState({ active: "explore" }) }}>
                 <img src={require("./images/logos/logo.png")} alt="" className="" height="48px" />
               </button>
-              <button className="d-sm-none border-0 bg-none" onClick={() => { this.setState({ navbarCollapsed: !this.state.navbarCollapsed }) }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+              <button className="d-sm-none border-0 bg-none" onClick={() => { this.setState({ navbarHidden: !this.state.navbarHidden }) }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" viewBox="0 0 16 16">
                   {
-                    this.state.navbarCollapsed ?
+                    this.state.navbarHidden ?
                       <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                       :
                       <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" />
                   }
                 </svg>
               </button>
-              <div className={"navbar-content container-fluid bg-light" + (this.state.navbarCollapsed ? " hide" : "")}>
-                <ul className="navbar-nav container-fluid d-flex justify-content-center">
-                  <li className="nav-item mx-auto mx-md-3 mx-lg-5 mb-2 mb-sm-0">
+              <div className={"navbar-content container-fluid bg-light" + (this.state.navbarHidden ? " hide" : "")}>
+                <ul className="navbar-nav container-fluid d-flex justify-content-md-evenly">
+                  <li className="nav-item mx-auto mx-md-0 mb-2 mb-sm-0">
                     <button
                       className={"nav-link border-0 bg-none pb-sm-0" + (this.state.active === "explore" ? " active" : "")}
                       onClick={() => { this.setState({ active: "explore" }) }}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-compass me-1" viewBox="0 0 16 16">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="me-1" viewBox="0 0 16 16">
                         {
                           this.state.active === "explore" ?
                             <path d="M15.5 8.516a7.5 7.5 0 1 1-9.462-7.24A1 1 0 0 1 7 0h2a1 1 0 0 1 .962 1.276 7.503 7.503 0 0 1 5.538 7.24zm-3.61-3.905L6.94 7.439 4.11 12.39l4.95-2.828 2.828-4.95z" />
@@ -95,7 +96,7 @@ class App extends React.Component {
                       className={"nav-link border-0 bg-none pb-sm-0" + (this.state.active === "create" ? " active" : "")}
                       onClick={() => { this.setState({ active: "create" }) }}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-plus-circle-fill me-1" viewBox="0 0 16 16">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="me-1" viewBox="0 0 16 16">
                         {
                           this.state.active === "create" ?
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
@@ -120,10 +121,31 @@ class App extends React.Component {
               </div>
             </div>
           </nav>
-          <main>
-            <h1>Hello World!</h1>
-          </main>
         </header>
+
+        <main className="d-flex">
+
+          <div className={"filter" + (this.state.filterHidden ? " hide" : "")}>
+            <h1>Filter</h1>
+
+          </div>
+
+          <div className="container-fluid bg-warning">
+            <h1>Hello World!</h1>
+          </div>
+
+          <button className="filter-btn d-lg-none" onClick={() => { this.setState({ filterHidden: !this.state.filterHidden }) }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" viewBox="0 0 16 16">
+              {
+                this.state.filterHidden ?
+                  <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+                  :
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+              }
+            </svg>
+          </button>
+
+        </main>
 
 
       </React.Fragment>
